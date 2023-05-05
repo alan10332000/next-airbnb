bb05f18a6ca9eb64ab8e743ef8df938575cb9c1f
Alan Chou <alan10332000@gmail.com>
2023-05-06 01:05:58 +0800

    feat: add navbar - container

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import Container from '../Container'
+
+const Navbar = () => {
+  return (
+    <div className="fixed z-10 w-full bg-white shadow-sm">
+      <div className="border-b py-4">
+        <Container></Container>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
