1f516e58dd773038bea192863fc0b5140805c874
Alan Chou <alan10332000@gmail.com>
2023-05-09 20:51:22 +0800

    fix: make the modal close on backdrop

--- app/components/Modal/Modal.tsx
+++ app/components/Modal/Modal.tsx
@@ -58,8 +58,14 @@ const Modal: React.FC<ModalProps> = ({
   if (!isOpen) return null
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-neutral-800/70 outline-none focus:outline-none">
-      <div className="relative mx-auto my-6 h-full w-full md:h-auto md:w-4/6 lg:h-auto lg:w-3/6 xl:w-2/5">
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-neutral-800/70 outline-none focus:outline-none"
+      onClick={handleClose}
+    >
+      <div
+        className="relative mx-auto my-6 h-full w-full md:h-auto md:w-4/6 lg:h-auto lg:w-3/6 xl:w-2/5"
+        onClick={(e) => e.stopPropagation()}
+      >
         {/*content*/}
         <div
           className={`h-full duration-300
