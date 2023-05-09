ba063be2caaacd34f94200e65d57b28027aa89a2
Alan Chou <alan10332000@gmail.com>
2023-05-09 15:25:04 +0800

    feat: feat: add listing detail - reservation

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,12 +1,22 @@
 'use client'
 
-import { useState, useMemo } from 'react'
+import { differenceInDays, eachDayOfInterval } from 'date-fns'
+import { useState, useEffect, useCallback, useMemo } from 'react'
+import { Range } from 'react-date-range'
 
 import Container from '@/app/components/Container'
 import ListingHead from '@/app/components/Listing/ListingHead'
 import ListingInfo from '@/app/components/Listing/ListingInfo'
+import ListingReservation from '@/app/components/Listing/ListingReservation'
 import { categories } from '@/app/components/Navbar/Categories'
+import useLoginModal from '@/app/hooks/useLoginModal'
 import { SafeListing, SafeReservation, SafeUser } from '@/app/types'
+
+const initialDateRange = {
+  startDate: new Date(),
+  endDate: new Date(),
+  key: 'selection',
+}
 
 interface ListingClientProps {
   listing: SafeListing & {
@@ -17,11 +27,45 @@ interface ListingClientProps {
 }
 
 const ListingClient: React.FC<ListingClientProps> = ({ listing, reservations = [], currentUser }) => {
+  const loginModal = useLoginModal()
   const [isLoading, setIsLoading] = useState(false)
+  const [totalPrice, setTotalPrice] = useState(listing.price)
+  const [dateRange, setDateRange] = useState<Range>(initialDateRange)
+
+  const disabledDates = useMemo(() => {
+    let dates: Date[] = []
+
+    reservations.forEach((reservation: any) => {
+      const range = eachDayOfInterval({
+        start: new Date(reservation.startDate),
+        end: new Date(reservation.endDate),
+      })
+
+      dates = [...dates, ...range]
+    })
+
+    return dates
+  }, [reservations])
 
   const category = useMemo(() => {
     return categories.find((items) => items.label === listing.category)
   }, [listing.category])
+
+  const onCreateReservation = useCallback(() => {
+    if (!currentUser) return loginModal.onOpen()
+  }, [currentUser, loginModal])
+
+  useEffect(() => {
+    if (dateRange.startDate && dateRange.endDate) {
+      const dayCount = differenceInDays(dateRange.endDate, dateRange.startDate)
+
+      if (dayCount && listing.price) {
+        setTotalPrice(dayCount * listing.price)
+      } else {
+        setTotalPrice(listing.price)
+      }
+    }
+  }, [dateRange, listing.price])
 
   return (
     <Container>
@@ -44,6 +88,17 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing, reservations = [
               bathroomCount={listing.bathroomCount}
               locationValue={listing.locationValue}
             />
+            <div className="order-first mb-10 md:order-last md:col-span-3">
+              <ListingReservation
+                price={listing.price}
+                totalPrice={totalPrice}
+                onChangeDate={(value) => setDateRange(value)}
+                dateRange={dateRange}
+                onSubmit={onCreateReservation}
+                disabled={isLoading}
+                disabledDates={disabledDates}
+              />
+            </div>
           </div>
         </div>
       </div>
