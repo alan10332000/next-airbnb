2c30f5fe584e7818595f31c62f2d2ca1dbd9fa49
Alan Chou <alan10332000@gmail.com>
2023-05-11 09:10:00 +0800

    feat: add modal - search

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import getCurrentUser from '@/app/actions/getCurrentUser'
 import LoginModal from '@/app/components/Modal/LoginModal'
 import RegisterModal from '@/app/components/Modal/RegisterModal'
 import RentModal from '@/app/components/Modal/RentModal'
+import SearchModal from '@/app/components/Modal/SearchModal'
 import Navbar from '@/app/components/Navbar/Navbar'
 import ToasterProvider from '@/app/providers/ToasterProvider'
 import '@/app/globals.css'
@@ -27,6 +28,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <LoginModal />
         <RegisterModal />
         <RentModal />
+        <SearchModal />
         <Navbar currentUser={currentUser} />
         <div className="pb-20 pt-28">{children}</div>
       </body>
